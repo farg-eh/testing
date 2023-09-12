8e97c67dfa662665c8f773b07d242af14572f32b
farg-eh <fargezbz@gmail.com>
2023-09-12 09:09:36 +0300

    first commit

---
+++ button.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Button:
+    def __init__(self, pos, name):
+        self.screen = pygame.display.get_surface()
+        self.pos = pos
+        self.name = name
+        self.size = (120, 90)
+        
+        self.surf = pygame.Surface(self.size)
+        self.rect = self.surf.get_rect(center=pos)
+        self.color = "white"
+        pygame.draw.rect(self.surf,self.color,
+        (0,0,self.size[0], self.size[1]), border_radius=30)
+        # self.surf.fill(self.color)
+        
+        self.font = pygame.font.Font(None, 45)
+        self.text = self.font.render(self.name, 1, "black")
+        self.text_rect = self.text.get_rect(center=pos)
+        
+        
+    def check_click(self, pos):
+        x, y = self.pos
+        w, h = self.surf.get_size()
+        
+        if x+w >= pos[0] >= x and y+h >= pos[1] >= y:
+            return self.name
+            
+    
+    def draw(self):
+        self.screen.blit(self.surf, self.rect)
+        self.screen.blit(self.text, self.text_rect)
+        
+        
+        
+    def update(self, dt):
+        self.draw()
---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame, sys
+from button import Button
+
+pygame.init()
+
+screen = pygame.display.set_mode((0, 0))
+SW, SH = screen.get_size()
+clock = pygame.time.Clock()
+
+btn = Button(pos=(SW/11, SH/20), name="Exit")
+
+font = pygame.font.Font(None, 30)
+screen_res = f"screen width : {SW}  screen height : {SH}"
+while True:
+    dt = clock.tick(60) / 1000
+    screen.fill("black")
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if btn.check_click(pos) == "Exit":
+                btn.surf.fill("red")  
+                pygame.quit()
+                sys.exit()
+                
+    # calculate fps
+    fps = str(int(clock.get_fps()))
+    
+    # print text
+    text= [screen_res, fps]
+    pos = SW/9, SH - SH/3
+    for i in range(len(text)):
+        new_pos = pos[0], pos[1] + i*45        
+        screen.blit(font.render(text[i],1,"red"), new_pos)
+    
+            
+            
+    # updating and drawing things
+    btn.update(dt)
+            
+    pygame.display.update()
+    
