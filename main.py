0d052d49c19430bbe072ba459ca4633aa5743462
farg-eh <fargezbz@gmail.com>
2023-09-12 09:56:43 +0300

    pink

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ btn = Button(pos=(SW/11, SH/20), name="Exit")
 
 font = pygame.font.Font(None, 30)
 screen_res = f"screen width : {SW}  screen height : {SH}"
+
 while True:
     dt = clock.tick(60) / 1000
     screen.fill("black")
@@ -21,6 +22,16 @@ while True:
                 btn.surf.fill("red")  
                 pygame.quit()
                 sys.exit()
+                
+        if event.type == pygame.FINGERDOWN:
+            screen.fill("brown")
+            
+        if event.type == pygame.FINGERUP:
+            screen.fill("green")
+            
+        if event.type == pygame.FINGERMOTION:
+            screen.fill("purple")
+                
                 
     # calculate fps
     fps = str(int(clock.get_fps()))
